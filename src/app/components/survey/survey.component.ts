55662ef864b61e829e628c268557b2a58752298c
daiana.bassi@gosh.nhs.uk <daiana.bassi.17@ucl.ac.uk>
2020-10-21 22:34:24 +0100

    bbb

--- src/app/components/survey/survey.component.ts
+++ src/app/components/survey/survey.component.ts
@@ -28,13 +28,8 @@ export class SurveyComponent implements OnInit {
 
     survey.onComplete.add( (result) => {
       this.data.changeMessage(result.getAllValues())
-      console.log(JSON.stringify({email: result.getValue('email'), fname: 'x', lname: 'x'}));
-<<<<<<< HEAD
-      this.http.post('https://menopause-assessment.herokuapp.com/api/signup', { email: result.getValue('email'), fname: result.getValue('name') , lname: 'x' }).subscribe(response => {
-=======
       this.completed = true;
       this.http.post('https://menopause-assessment.herokuapp.com/api/signup', { email: result.getValue('email'), fname: 'x', lname: 'x' }).subscribe(response => {
->>>>>>> heroku_deploy
         console.log(response)
       });
      console.log('this is result' +  JSON.stringify(result.getAllValues()));
