c675e12d526a621b38ce7f9080d27c7e3e04eda1
db-assi <daianabassi1@gmail.com>
2021-01-23 15:05:35 +0000

    removed all previous styling

--- src/app/components/survey/survey.component.ts
+++ src/app/components/survey/survey.component.ts
@@ -4,46 +4,36 @@ import * as Survey from 'survey-angular';
 import { SurveyService } from 'src/app/core/services/survey/survey.service';
 import json from '../../../assets/data/surveyjs-model.json';
 
-
+import 'bootstrap/dist/css/bootstrap.css';
+import "survey-angular/survey.css";
+Survey.StylesManager.applyTheme("bootstrap");
 
 @Component({
-  selector: 'ng-app',
+  selector: 'survey',
   templateUrl: './survey.component.html',
   styleUrls: ['./survey.component.css']
 })
 
 export class SurveyComponent implements OnInit {
-
+  survey: Survey.Model;
   isCompleted: boolean = false;
   isEarlyAge: boolean = false;
 
-  constructor(private survey: SurveyService) {}
+  constructor
+    (
+       private service: SurveyService
+    ) {}
 
   ngOnInit(): void {
-
     Survey
       .SurveyNG
-      .render('surveyElement',
-        { 
-          model: this.survey.createSurveyModel(json),
-          onComplete: this.survey.onCompleteSurvey
-        });
+        .render('surveyElement',
+          {
+            model: this.service.createSurveyModel(json),
+            onComplete: this.service.onCompleteSurvey
+          });
 
-    Survey.StylesManager.applyTheme("bootstrap");
-    
-
-
-    this.survey.currentEarlyAge.subscribe(status => {
-      this.isEarlyAge = status
-    })
-
-    this.survey.currentCompleteStatus.subscribe(status => {
-      this.isCompleted = status
-    });
-
+    this.service.currentEarlyAge.subscribe(status => {this.isEarlyAge = status});
+    this.service.currentCompleteStatus.subscribe(status => {this.isCompleted = status});
   }
-
-
-
-
 }
